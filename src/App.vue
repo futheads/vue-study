db3e102366ce38c008de683d0f288b70ff1496b2
futhead <futhead@163.com>
2018-08-11 14:14:41 +0800

    实现组件拆分

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
+import header from './components/header/header.vue'
+
 export default {
-  name: 'App'
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  .tab
+      display flex
+      width 100%
+      height 40px
+      line-height 40px
+      .tab-item
+          flex 1
+          text-align center
 </style>
