6d211af95cff970ddd4fc5c3ea53b42e1ede7f4a
futhead <futhead@163.com>
2018-08-12 09:41:32 +0800

    实现header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab">
       <div class="tab-item">商品</div>
       <div class="tab-item">评论</div>
@@ -15,21 +15,53 @@
 <script>
 import header from './components/header/header.vue'
 
+const ERR_OK = 0
+
 export default {
+  data () {
+    return {
+      seller: {}
+    }
+  },
+
+  created() {
+    this.$http.get('api/seller').then((response) => {
+      response = response.body
+      if (response.errno === ERR_OK) {
+        this.seller = response.data
+      }
+    })
+  },
+
   components: {
     'v-header': header
   }
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-#app
-  .tab
-      display flex
-      width 100%
-      height 40px
-      line-height 40px
-      .tab-item
-          flex 1
-          text-align center
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "common/scss/mixins";
+
+  .tab {
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    @include border-1px(rgba(7, 17, 27, 0.1));
+
+    .tab-item {
+      flex: 1;
+      text-align: center;
+
+      & > a {
+        display: block;
+        font-size: 14px;
+        color: rgb(77, 85, 93);
+
+        &.active {
+          color: rgb(240, 20, 20)
+        }
+      }
+    }
+  }
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,283 @@
 <template>
     <div class="header">
-      我是 header
+      <div class="content-wrapper">
+        <div class="avatar">
+          <img width="64" height="64" :src="seller.avatar"/>
+        </div>
+        <div class="content">
+          <div class="title">
+            <span class="brand"></span>
+            <span class="name">{{ seller.name }}</span>
+          </div>
+          <div class="description">
+            {{ seller.description }}/{{ seller.deliveryTime }}分钟到达
+          </div>
+          <div v-if="seller.supports" class="support">
+            <span class="icon"></span>
+            <span class="text">{{ seller.supports[0].description }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="bulletin-wrapper"></div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  }
+}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+  @import "../../common/scss/mixins";
 
+  .header {
+    position: relative;
+    color: #fff;
+    background: rgba(7, 17, 27, 0.5);
+    overflow: hidden;
+    .content-wrapper {
+      position: relative;
+      padding: 24px 12px 18px 24px;
+      font-size: 0;
+      .avatar {
+        display: inline-block;
+        vertical-align: top;
+        img {
+          border-radius: 2px;
+        }
+      }
+      .content {
+        display: inline-block;
+        margin-left: 16px;
+        .title {
+          margin: 2px 0 8px 0;
+          .brand {
+            width: 30px;
+            height: 18px;
+            display: inline-block;
+            @include bg-image('brand');
+            background-size: 30px 18px;
+            background-repeat: no-repeat;
+            vertical-align: top;
+          }
+          .name {
+            margin-left: 6px;
+            font-size: 16px;
+            line-height: 18px;
+            font-weight: bold;
+          }
+        }
+        .description {
+          margin-bottom: 10px;
+          line-height: 12px;
+          font-size: 12px;
+        }
+        .support {
+          .icon {
+            display: inline-block;
+            vertical-align: top;
+            width: 12px;
+            height: 12px;
+            margin-right: 4px;
+            background-size: 12px 12px;
+            background-repeat: no-repeat;
+            &.decrease {
+              @include bg-image('decrease_1');
+            }
+            &.discount {
+              @include bg-image('discount_1');
+            }
+            &.guarantee {
+              @include bg-image('guarantee_1');
+            }
+            &.invoice {
+              @include bg-image('invoice_1');
+            }
+            &.special {
+              @include bg-image('special_1');
+            }
+          }
+          .text {
+            line-height: 12px;
+            font-size: 10px;
+          }
+        }
+      }
+      .support-content {
+        position: absolute;
+        right: 12px;
+        bottom: 14px;
+        padding: 0 8px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 14px;
+        background: rgba(0, 0, 0, .2);
+        text-align: center;
+        .count {
+          vertical-align: top;
+          font-size: 10px;
+        }
+        .icon-keyboard_arrow_right {
+          margin-left: 2px;
+          line-height: 24px;
+          font-size: 10px;
+        }
+      }
+    }
+    .bulletin-wrapper {
+      position: relative;
+      height: 28px;
+      line-height: 28px;
+      padding: 0 22px 0 12px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      background: rgba(7, 17, 27, .2);
+      .bulletin-title {
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 8px;
+        width: 22px;
+        height: 12px;
+        @include bg-image('bulletin');
+        background-size: 22px 12px;
+        background-repeat: no-repeat;
+      }
+      .bulletin-text {
+        vertical-align: top;
+        margin: 0 4px;
+        font-size: 10px;
+      }
+      .icon-keyboard_arrow_right {
+        position: absolute;
+        font-size: 10px;
+        right: 12px;
+        top: 8px;
+      }
+    }
+    .background {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      filter: blur(10px);
+    }
+    .detail {
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      transition: all .5s;
+      background: rgba(7, 17, 27, .8);
+      backdrop-filter: blur(10px); /*ios 模糊背景*/
+      &.fade-enter-active, &.fade-leave-active {
+        transition: opacity .5s
+      }
+      &.fade-enter, &.fade-leave-active {
+        opacity: 0
+      }
+      .detail-wrapper {
+        width: 100%;
+        min-height: 100%;
+        .detail-main {
+          margin-top: 64px;
+          padding-bottom: 64px; /*sticky footer 必须有  高度为需要定位元素所占的高度*/
+          .name {
+            line-height: 16px;
+            text-align: center;
+            font-size: 16px;
+            font-weight: 700;
+          }
+          .star-wrapper {
+            margin-top: 18px;
+            padding: 2px 0;
+            text-align: center;
+          }
+          .title {
+            display: flex;
+            width: 80%;
+            margin: 28px auto 24px;
+            .line {
+              flex: 1;
+              position: relative;
+              top: -6px;
+              border-bottom: 1px solid rgba(255, 255, 255, .2);
+            }
+            .text {
+              padding: 0 12px;
+              font-weight: 700;
+              font-size: 14px;
+            }
+          }
+          .supports {
+            width: 80%;
+            margin: 0 auto;
+            .support-item {
+              padding: 0 12px;
+              margin-bottom: 12px;
+              font-size: 0;
+              &:last-child {
+                margin-bottom: 0;
+              }
+              .icon {
+                display: inline-block;
+                width: 16px;
+                height: 16px;
+                vertical-align: top;
+                margin-right: 6px;
+                background-size: 16px 16px;
+                background-repeat: no-repeat;
+                &.decrease {
+                  @include bg-image('decrease_2');
+                }
+                &.discount {
+                  @include bg-image('discount_2');
+                }
+                &.guarantee {
+                  @include bg-image('guarantee_2');
+                }
+                &.invoice {
+                  @include bg-image('invoice_2');
+                }
+                &.special {
+                  @include bg-image('special_2');
+                }
+              }
+              .text {
+                font-size: 12px;
+                line-height: 16px;
+              }
+            }
+          }
+          .bulletin {
+            width: 80%;
+            margin: 0 auto;
+            .content{
+              padding: 0 12px;
+              line-height: 24px;
+              font-size: 12px;
+            }
+          }
+        }
+      }
+      .detail-close {
+        position: relative;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0; /*关键*/
+        clear: both; /*关键*/
+        font-size: 32px;
+      }
+    }
+  }
 </style>
